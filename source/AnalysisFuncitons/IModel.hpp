7c8e36c2ce0072f1993dc2c765175da2e6b660e2
Fizka <nonuuxx@hotmail.com>
2025-04-17 18:47:21 +0100

    Added IModel interface that all models should inherit from. This interface exposes the methods necessary by ModelCoordinator in order to populate the models with weapons

---
+++ source/AnalysisFuncitons/IModel.hpp
@@ -0,0 +1,10 @@
+#include "../WeaponCore/Weapon.hpp"
+class IModel {
+public:
+    virtual ~IModel() = default;
+    // Process each weapon once as part of an ongoing pass:
+    virtual void updateWithWeapon(const Weapon& w) = 0;
+    virtual void processCurrentType() = 0;
+    // Once all weapons have been processed, finalize the fitting:
+    virtual void finalize() = 0;
+};
